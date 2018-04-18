02bb32926ccfd62f72705a04e8aeef0923f9a992
alhanson7210 <alhanson@dons.usfca.edu>
2018-04-17 17:15:06 -0700

    first iteration of the e-club developer's website

---
+++ website/js/googleform.js
@@ -0,0 +1,6 @@
+var form = FormApp.create('New Form');
+var pageBreak = form.addPageBreakItem().setTitle('Welcome to E-club!');
+var textItem = form.addTextItem().setTitle('Add your school email:');
+
+pageBreak;
+textItem;
