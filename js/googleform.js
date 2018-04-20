9636fc593c82fac2efac3f8ed5ea3bab7c9a3736
alhanson7210 <alhanson@dons.usfca.edu>
2018-04-19 17:21:42 -0700

    bootstrap setup with e-club information from powerpoint

--- js/googleform.js
+++
@@ -1,6 +0,0 @@
-var form = FormApp.create('New Form');
-var pageBreak = form.addPageBreakItem().setTitle('Welcome to E-club!');
-var textItem = form.addTextItem().setTitle('Add your school email:');
-
-pageBreak;
-textItem;
